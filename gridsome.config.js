5959e48f3ff14c9275bbe48a9c1e031d6931818a
LJ <ljmarinscull@gmail.com>
2020-09-12 21:10:06 -0400

    [delete] Unused files

--- gridsome.config.js
+++ gridsome.config.js
@@ -8,5 +8,15 @@ module.exports = {
   siteName: 'Caribbean Coding Academy Grenada',
   siteDescription:'The CCA aims to teach our candidates all the necessary skills that they would need to work in a specific area within the software or Information Technology industry.',
   siteUrl: 'https://ljmarinscull.github.io',
-  plugins: []
+  plugins: [
+    {
+      use: "gridsome-plugin-service-worker",
+      options: {
+        networkFirst:{
+          routes: ["/", "/projects", "/services","/team","/contact"],
+          fileTypes: ["document", "script", "style", "image"],
+        },
+      },
+    },
+  ]
 }
