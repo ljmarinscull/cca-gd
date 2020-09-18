5460e5556d214acf16957a93dbf10513e540b1ac
LJ <ljmarinscull@gmail.com>
2020-09-18 11:26:53 -0400

    [delete] Delete unused files

--- src/.temp/config.js
+++
@@ -1,8 +0,0 @@
-export default {
-  "trailingSlash": true,
-  "pathPrefix": "",
-  "titleTemplate": "%s - Caribbean Coding Academy Grenada",
-  "siteUrl": "https://ljmarinscull.github.io",
-  "version": "0.7.20",
-  "catchLinks": true
-}
